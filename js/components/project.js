05d5ff909e14d467c9d43a34c4ff43c21861d64d
Esteban Salazar <esteban.salazar@pragma.com.co>
2017-07-05 08:47:17 -0500

    Fix error in controller

--- js/components/project.js
+++ js/components/project.js
@@ -3,18 +3,21 @@ angular.module('project', ['ngRoute', 'firebase'])
 .config(function($routeProvider) {
 
 
-    $routeProvider
-        .when('/', {
-            controller: 'ProjectController as projectctrl',
-            templateUrl: 'main.html'
-                //resolve: resolveProjects
-        })
-        // .when('/edit/:projectId', {
-        //   controller:'EditProjectController as editProject',
-        //   templateUrl:'detail.html',
-        //   resolve: resolveProjects
-        // })
-        .otherwise({
-            redirectTo: '/'
-        });
-});
+        $routeProvider
+            .when('/', {
+                controller: 'ProjectController as projectctrl',
+                templateUrl: 'main.html'
+                    //resolve: resolveProjects
+            })
+            // .when('/edit/:projectId', {
+            //   controller:'EditProjectController as editProject',
+            //   templateUrl:'detail.html',
+            //   resolve: resolveProjects
+            // })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('ProjectController', function(projects) {
+        var vm = this;
+    });
